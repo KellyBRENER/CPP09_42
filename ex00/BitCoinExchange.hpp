5c728a5a3278dc1582b41597a077fb3473f735f5
kishoun <kellybrener66@gmail.com>
2024-12-02 16:35:09 +0100

    ex00_inprogress

---
+++ ex00/BitCoinExchange.hpp
@@ -0,0 +1,22 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitCoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbrener- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 15:18:09 by kbrener-          #+#    #+#             */
+/*   Updated: 2024/12/02 15:19:16 by kbrener-         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include <iostream>
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+class	BitCoinExchange {
+	
+};
+
+#endif
